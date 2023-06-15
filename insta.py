8aca357a4a63f9407ad594a11aa1938394e2bce0
Pranav Dileep <91619846+pranavkdileep@users.noreply.github.com>
2023-06-15 19:31:02 +0530

    Update insta.py

--- insta.py
+++ insta.py
@@ -20,3 +20,8 @@ def echo_all(message):
 
 bot.infinity_polling()
 app = Flask(__name__)
+@app.route('/')
+    def index():
+        return 'Web App with Python Flask!'
+    
+   app.run(host='0.0.0.0', port=81)
